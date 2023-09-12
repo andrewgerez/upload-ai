1096802074c8d7995bbedf213e4f2b68a9453e6c
Andrew Gerez <andrewgerez18@gmail.com>
2023-09-11 21:27:06 -0300

    feat: ui to load video in sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
-import { Github } from 'lucide-react'
+import { Github, FileVideo } from 'lucide-react'
 import { Button } from "./components/ui/button";
 import { Separator } from './components/ui/separator';
 import { Textarea } from './components/ui/textarea';
+import { Label } from './components/ui/label';
 
 export function App() {
   return (
@@ -43,7 +44,45 @@ export function App() {
             no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.
           </p>
         </div>
-        <aside className="w-80"></aside>
+
+        <aside className="w-80 space-y-6">
+          <form className="space-y-6">
+            <label 
+              htmlFor="video"
+              className="border 
+                flex rounded-md aspect-video cursor-pointer 
+                border-dashed text-sm flex-col gap-2 items-center 
+                justify-center text-muted-foreground
+                hover:bg-primary/5"
+            >
+              <FileVideo className="w-4 h-4" />
+              Selecione um video
+            </label>
+
+            <input 
+              type="file" 
+              id="video" 
+              accept="video/mp4" 
+              className="sr-only" 
+            />
+
+            <Separator />
+
+            <div className="space-y-1">
+              <Label 
+                htmlFor="transcription_prompt"
+              >
+                Prompt de transcrição
+              </Label>
+
+              <Textarea 
+                id="transcription_prompt"
+                className="min-h-20 leading-relaxed"
+                placeholder="Inclua palavras-chave mencionadas no video separadas por vírgula (,)"
+              />
+            </div>
+          </form>
+        </aside>
       </main>
     </div>
   )
