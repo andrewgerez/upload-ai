9c9ed160cdf4612857b3380c6c11a6b4c282f5bb
Andrew Gerez <andrewgerez18@gmail.com>
2023-09-13 21:15:21 -0300

    feat: ui for upload status

--- src/components/ui/video-input-form.tsx
+++ src/components/ui/video-input-form.tsx
@@ -8,8 +8,11 @@ import { getFFmpeg } from "@/lib/ffmpeg"
 import { fetchFile } from "@ffmpeg/util"
 import { api } from "@/lib/axios"
 
+type Status = 'waiting' | 'converting' | 'uploading' | 'generating' | 'success'
+
 export const VideoInputForm = () => {
   const [videoFile, setVideoFile] = useState<File | null>(null)
+  const [status, setStatus] = useState<Status>('waiting')
   const promptInputRef = useRef<HTMLTextAreaElement>(null)
 
   const handleFileSelected = (e: ChangeEvent<HTMLInputElement>) => {
@@ -72,21 +75,27 @@ export const VideoInputForm = () => {
       return
     }
 
+    setStatus('converting')
+
     const audioFile = await convertVideoToAudio(videoFile)
 
     const data = new FormData()
 
     data.append('file', audioFile)
 
+    setStatus('uploading')
+
     const response = await api.post('/videos', data)
 
     const videoId = response.data.video.id
+
+    setStatus('generating')
 
     await api.post(`/videos/${videoId}/transcription`, {
       prompt,
     })
 
-    console.log('finished.')
+    setStatus('success')
   }
 
   const previewURL = useMemo(() => {
@@ -136,13 +145,14 @@ export const VideoInputForm = () => {
 
         <Textarea
           ref={promptInputRef}
+          disabled={status !== 'waiting'}
           id="transcription_prompt"
           className="h-20 leading-relaxed resize-none"
           placeholder="Inclua palavras-chave mencionadas no video separadas por vírgula (,)"
         />
       </div>
 
-      <Button type="submit" className="w-full">
+      <Button disabled={status !== 'waiting'} type="submit" className="w-full">
         Carregar video
         <Upload className="w-4 h-4 ml-2" />
       </Button>
